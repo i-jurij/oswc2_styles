79fa29bd5e3865ef19d7bb4c3b6d0ed77126e6af
I-Jurij <yjurij@gmail.com>
2024-12-10 22:54:00 +0300

    ch

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,11 +2,11 @@ import css from 'rollup-plugin-css-porter';
 
 export default {
 	input: 'src/app.js',
-  	plugins: [ css({
-   	raw: 'oswc.css',
-    	minified: 'oswc.min.css',
-  	}) ],
-  	output: [
+	plugins: [css({
+		raw: 'oswc2_styles.css',
+		minified: 'oswc2_styles.min.css',
+	})],
+	output: [
 		{
 			file: './build/app.js',
 			format: 'es'
