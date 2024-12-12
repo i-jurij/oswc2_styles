da3e9fe300f904117f0fd6bdee281e3f0d2d84d6
I-Jurij <yjurij@gmail.com>
2024-12-13 00:42:05 +0300

    upd

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import './modules/picnic/picnic.css';
+
 import './modules/type-scale/index.css';
 import './modules/typography/index.css';
 import './modules/layout/index.css';
@@ -9,7 +11,9 @@ import './modules/flexbox/index.css';
 import './modules/position/index.css';
 import './modules/border/index.css';
 import './modules/hide/index.css';
-import './modules/picnic/picnic.css';
+
 import './modules/mrmrs-colors/colors.css';
+
 import './modules/root.css';
+
 console.info('Ok');
